92d000d53c716214a109b9eb3f2bdd2f071e06c2
XxxiaoyuGIT <xiaoyu.shi@my.jcu.edu.au>
2023-11-21 20:44:33 +1000

    Ass2 Record the song and mark whether to learn or not

--- song.py
+++ song.py
@@ -1,9 +1,29 @@
 """..."""
-
-
-# TODO: Create your Song class in this file
-
-
 class Song:
-    """..."""
-    pass
+    def __init__(self, title="", artist="", year=0, is_learned=False):
+        """Constructor for the Song class."""
+        self.title = title
+        self.artist = artist
+        self.year = year
+        self.is_learned = is_learned
+
+    def to_dict(self):
+        """Convert the Song object to a dictionary."""
+        return {
+            'title': self.title,
+            'artist': self.artist,
+            'year': self.year,
+            'is_learned': self.is_learned
+        }
+
+    def mark_as_unlearned(self):
+        """Mark the song as unlearned."""
+        self.is_learned = False
+
+    def mark_as_learned(self):
+        """Mark the song as learned."""
+        self.is_learned = True
+
+    def __str__(self):
+        """String representation of the Song object."""
+        return f"{self.title} by {self.artist}, Released: {self.year}, Learned: {self.is_learned}"
